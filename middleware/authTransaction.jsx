aa4ced93ddbe2fe1d9763afc57192b8f7a48907e
stresslo <stresslo.lab@gmail.com>
2024-02-27 22:01:58 +0700

    upload file

--- middleware/authTransaction.jsx
+++ middleware/authTransaction.jsx
@@ -135,7 +135,9 @@ const AuthTransaction = () => {
             </div>
             }
             <div className='title' style={{textAlign: 'center', marginTop: '20px'}}>Product</div>
-            {data.transaction_status == 'settlement' && <p style={{color: 'var(--blue)', textAlign: 'center', cursor: 'pointer'}}>*Screenshot if needed</p>}
+            {(data) && 
+                data.transaction_status == 'settlement' && <p style={{color: 'var(--blue)', textAlign: 'center', cursor: 'pointer'}}>*Screenshot if needed</p>
+            }
             <div style={{width: '100%', display: 'flex', gap: '5px', fontFamily: 'var(--quicksand)'}}>
                 <div style={{width: '50%', display: 'flex', flexDirection: 'column', gap: '5px', color: 'var(--yellow)'}}>
                     <h4 style={{border: '1px solid var(--blue)', padding: '10px', borderRadius: '5px'}}>Customer :</h4>
