aa144d6b1d64b65c3ed7f2ad9fc27177842ba88f
stresslo <stresslo.lab@gmail.com>
2024-02-29 16:39:49 +0700

    upload file

--- utils/randomize.js
+++ utils/randomize.js
@@ -2,15 +2,15 @@ import { v4 as uuid } from "uuid"
 
 const randomize = (number) => {
   const scramble = uuid().replace(/-/g,'')
-  return `VX${scramble.slice(0, number || 20)}`
+  return `${scramble.slice(0, number || 20)}`
 };
 
 const changeclass = () => {
   const element = document.querySelectorAll('[class]')
   element.forEach(result => {
     const currentclass = result.className
-    const random = randomize(100)
-    const newclass = `${random} ${currentclass}`
+    const random = randomize(20)
+    const newclass = `${random}-${random} ${currentclass}`
     result.className = newclass
   })
 }
