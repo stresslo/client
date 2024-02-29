c37f87bd8ae1aa88ccba1ec17c730e425a9b9c0d
stresslo <stresslo.lab@gmail.com>
2024-02-29 18:26:04 +0700

    upload file

--- data/stresslo.jsx
+++ data/stresslo.jsx
@@ -42,6 +42,36 @@ const stresslo =
         ]
     },
     {
+        ctg: "Upcoming on",
+        data : 
+        [
+            {
+                title : "3D design",
+                text : "assets 3d for your projects",
+                img : "img/3d.png",
+                color : "var(--yellow)",
+                pricing : ["paid"],
+                ctg : "3d"
+            },
+            {
+                title : "Motion graphic",
+                text : "Motion logo templates",
+                img : "img/motion.png",
+                color : "#FAEDCA",
+                pricing : ["paid", "free"],
+                ctg : "motion"
+            },
+            {
+                title : "Vector design",
+                text : "find vector assets template",
+                img : "img/vector.png",
+                color : "#FF4B63",
+                pricing : ["paid", "free"],
+                ctg : "vector"
+            },
+        ]
+    },
+    {
         ctg : "How to download on",
         data : 
         [
