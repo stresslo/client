6f8df6daf24ad3830f6c14d7ac421c2ced2fd325
stresslo <stresslo.lab@gmail.com>
2024-03-02 02:58:38 +0700

    upload file

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -61,7 +61,7 @@ const Product = () => {
             <div className='form' style={{marginTop: '60px', marginBottom: '0', paddingBottom: '0'}}>
                 <div style={{width: '100%', display: 'flex', alignItems: 'center', position: 'relative', gap: '5px'}}>
                     <input type="text" onChange={(e) => setValue(e.target.value)} placeholder='search product' className='search' style={{width: '100%', backgroundColor: 'unset', boxShadow: 'unset', border: '2px solid var(--primary)'}}/>
-                    <div onClick={() => searchProduct()} className='button' style={{width: '45px', height: '45px', backgroundColor: 'var(--primary)'}}>
+                    <div onClick={() => searchProduct()} className='button' style={{width: '90px', height: '45px', backgroundColor: 'var(--primary)'}}>
                         <div className='fa-solid fa-close fa-lg' style={{color: 'var(--text)'}}/>
                     </div>
                 </div>
