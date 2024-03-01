7dfb00bbfe29a9d2a8a6d22d4acb32444203ff37
stresslo <stresslo.lab@gmail.com>
2024-03-01 20:20:18 +0700

    upload file

--- src/pages/contributor/store.jsx
+++ src/pages/contributor/store.jsx
@@ -41,7 +41,7 @@ const Store = () => {
             <div className='product-page' style={{paddingBottom: '0'}}>
                 <div style={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '10px', marginBottom: '50px'}}>
                     <div className='itext' style={{color: 'var(--yellow)'}}>Status</div>
-                    <div style={{marginTop: '5px', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '20px', fontFamily: 'var(--quicksand)', fontSize: '0.85rem'}}>
+                    <div style={{marginTop: '5px', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '20px', fontFamily: 'var(--quicksand)', fontSize: '0.75rem'}}>
                         <div className='button' onClick={() => setStatus('active')} style={{backgroundColor: 'var(--primary)', height: '35px', color: 'var(--green)'}}>Active</div>
                         <div className='button' onClick={() => setStatus('pending')} style={{backgroundColor: 'var(--primary)', height: '35px', color: 'var(--blue)'}}>Pending</div>
                         <div className='button' onClick={() => setStatus('rejected')} style={{backgroundColor: 'var(--primary)', height: '35px', color: 'var(--oren)'}}>Rejected</div>
