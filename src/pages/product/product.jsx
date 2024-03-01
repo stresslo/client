fa70cd9a90ce6a4dbe3cd75ef1c8eaa0517257f9
stresslo <stresslo.lab@gmail.com>
2024-03-02 03:01:45 +0700

    upload file

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -55,14 +55,14 @@ const Product = () => {
                     <div className="fa-solid fa-arrow-left fa-xl active"></div>
                     <div className="nav-logo"><h1>stresslo</h1></div>
                 </div>
-                <div style={{position: 'absolute', top: '20px', right: '30px', display: 'flex', alignItems: 'center'}}>
+                <div style={{position: 'absolute', top: '30px', right: '30px', display: 'flex', alignItems: 'center'}}>
                     <div className='fa-solid fa-search fa-xl' style={{color: 'var(--text)'}}></div>
                 </div>
             <div className='form' style={{marginTop: '60px', marginBottom: '0', paddingBottom: '0'}}>
                 <div style={{width: '100%', display: 'flex', alignItems: 'center', position: 'relative', gap: '5px'}}>
                     <input type="text" onChange={(e) => setValue(e.target.value)} placeholder='search product' className='search' style={{width: '100%', backgroundColor: 'unset', boxShadow: 'unset', border: '2px solid var(--primary)'}}/>
-                    <div onClick={() => searchProduct()} className='button' style={{width: '90px', height: '45px', backgroundColor: 'var(--primary)'}}>
-                        <div className='fa-solid fa-close fa-lg' style={{color: 'var(--text)'}}/>
+                    <div onClick={() => searchProduct()} className='button' style={{width: '80px', height: '45px', backgroundColor: 'var(--primary)'}}>
+                        <div className='fa-solid fa-close fa-xl' style={{color: 'var(--text)'}}/>
                     </div>
                 </div>
             </div>
