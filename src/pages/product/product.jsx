d8a7d1a2309f574d9928578015f0d0877be4bd29
stresslo <stresslo.lab@gmail.com>
2024-03-01 23:37:04 +0700

    upload file

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -39,11 +39,11 @@ const Product = () => {
                     <div className="fa-solid fa-arrow-left fa-xl active"></div>
                     <div className="nav-logo"><h1>stresslo</h1></div>
                 </div>
-            <div className='form'>
-                <div style={{width: '100%', display: 'flex', justifyContent: 'space-between', gap:'10px', alignItems: 'center', marginBottom: '50px', position: 'relative'}}>
+            <div className='form' style={{margin: '20px 0'}}>
+                <div style={{width: '100%', display: 'flex', justifyContent: 'space-between', gap:'10px', alignItems: 'center', position: 'relative'}}>
                     <div style={{display: 'flex', alignItems: 'center', gap: '15px'}}>
                         <input type="text" className='search'/>
-                        <div className='fa-solid fa-search fa-xl' style={{color: 'var(--yellow)'}}/>
+                        <div className='fa-solid fa-search fa-xl' style={{color: 'var(--yellow)', cursor: 'pointer'}}/>
                     </div>
                 </div>
             </div>
