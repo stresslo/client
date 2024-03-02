33f3ae740b671d29e8e6be57f7bf053fa4b41299
stresslo <stresslo.lab@gmail.com>
2024-03-02 14:26:15 +0700

    upload file

--- src/pages/product/filterbox.jsx
+++ src/pages/product/filterbox.jsx
@@ -6,13 +6,13 @@ const FilterBox = () => {
             <div onClick={() => { document.querySelector('.filter-box').classList.remove('show') }} className="fa-solid fa-close fa-xl" style={{position : 'absolute', top: '25px', right: '20px', color: 'var(--text)', cursor: 'pointer'}}/>
             <div style={{width: '100%', height: 'max-content', overflow: 'hidden scroll'}}>
                 <div className="itext" style={{fontSize: '1rem'}}>Framework</div>
-                <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px'}}>
-                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>React</div>
-                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Angular</div>
-                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Html & CSS</div>
-                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Vue</div>
-                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Svelte</div>
-                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Next JS</div>
+                <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px', marginTop: '10px'}}>
+                    <div className="button" style={{borderRadius: '10px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)'}}>React</div>
+                    <div className="button" style={{borderRadius: '10px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)'}}>Angular</div>
+                    <div className="button" style={{borderRadius: '10px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)'}}>Html & CSS</div>
+                    <div className="button" style={{borderRadius: '10px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)'}}>Vue</div>
+                    <div className="button" style={{borderRadius: '10px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)'}}>Svelte</div>
+                    <div className="button" style={{borderRadius: '10px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)'}}>Next JS</div>
                 </div>
             </div>
         </div>
