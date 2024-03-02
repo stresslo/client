8f2ff4701b04d398f4b1d4a761703668337b9cad
stresslo <stresslo.lab@gmail.com>
2024-03-02 15:15:01 +0700

    upload file

--- src/pages/product/filterbox.jsx
+++ src/pages/product/filterbox.jsx
@@ -9,7 +9,7 @@ const FilterBox = () => {
     return (
         <div className='filter-box'>
             <div onClick={() => { document.querySelector('.filter-box').classList.remove('show') }} className="fa-solid fa-close fa-xl" style={{position : 'absolute', top: '25px', right: '20px', color: 'var(--text)', cursor: 'pointer'}}/>
-            <div style={{width: '100%', height: 'max-content', overflow: 'hidden scroll'}}>
+            <div style={{width: '100%', height: 'max-content'}}>
                 <div className="itext" style={{fontSize: '1.2rem', color: 'var(--blue)'}}>Pricing</div>
                 <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px', padding: '15px 0px', paddingBottom: '30px'}}>
                     <div className="button" style={ pricing === 'free' ? {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)', color : 'var(--green)'} : {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'unset', border: '1px solid var(--blue)', color : 'var(--blue)'}} onClick={() => setPricing('free')}>Free</div>
