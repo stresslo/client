45da0d8e28f31cb58b3f9c89ed28331535b6c187
stresslo <stresslo.lab@gmail.com>
2024-03-02 14:23:08 +0700

    upload file

--- src/pages/product/filterbox.jsx
+++ src/pages/product/filterbox.jsx
@@ -4,6 +4,17 @@ const FilterBox = () => {
     return (
         <div className='filter-box'>
             <div onClick={() => { document.querySelector('.filter-box').classList.remove('show') }} className="fa-solid fa-close fa-xl" style={{position : 'absolute', top: '25px', right: '20px', color: 'var(--text)', cursor: 'pointer'}}/>
+            <div style={{width: '100%', height: 'max-content', overflow: 'hidden scroll'}}>
+                <div className="itext" style={{fontSize: '1rem'}}>Framework</div>
+                <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px'}}>
+                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>React</div>
+                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Angular</div>
+                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Html & CSS</div>
+                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Vue</div>
+                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Svelte</div>
+                    <div className="button" style={{borderRadius: '10px', width: '80px', backgroundColor: '#aaa'}}>Next JS</div>
+                </div>
+            </div>
         </div>
     )
 }
