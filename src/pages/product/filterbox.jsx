a21a2f5e9587fbade491574a1fe72e07cfd3a624
stresslo <stresslo.lab@gmail.com>
2024-03-02 16:06:48 +0700

    upload file

--- src/pages/product/filterbox.jsx
+++ src/pages/product/filterbox.jsx
@@ -42,7 +42,7 @@ const FilterBox = () => {
                     <div className="button" style={ tech === 'next' ? {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)', color : 'var(--blue)'} : {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'unset', border: '1px solid var(--blue)', color : 'var(--blue)'}} onClick={() => setTech('next')}>Next JS</div>
                 </div>
                 <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', padding: '20px 0'}}>
-                    <div style={{width: '500px'}}><div className="button-max">Apply</div></div>
+                    <div style={{width: '500px'}}><div className="button-max" style={(tech || price || pricing ) ? {backgroundColor: 'var(--yellow)'} : ''}>Apply</div></div>
                 </div>
             </div>
         </div>
