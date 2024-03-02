0e7390881c4008f9142f41ea7e19e205e8a20022
stresslo <stresslo.lab@gmail.com>
2024-03-02 13:06:14 +0700

    upload file

--- src/pages/product/filterbox.jsx
+++ src/pages/product/filterbox.jsx
@@ -3,7 +3,7 @@ import "../../style/filterbox.css"
 const FilterBox = () => {
     return (
         <div className='filter-box'>
-            <div onClick={() => { document.querySelector('.filter-box').classList.remove('show') }} className="fa-solid fa-close fa-xl" style={{position : 'absolute', top: '10px', right: '15px', color: 'var(--text)', cursor: 'pointer'}}/>
+            <div onClick={() => { document.querySelector('.filter-box').classList.remove('show') }} className="fa-solid fa-close fa-xl" style={{position : 'absolute', top: '25px', right: '15px', color: 'var(--text)', cursor: 'pointer'}}/>
         </div>
     )
 }
