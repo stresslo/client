263a815bcc00ab4b93e00efada348ba10ae1ec3c
stresslo <stresslo.lab@gmail.com>
2024-03-04 03:02:32 +0700

    upload file

--- src/pages/product/order.jsx
+++ src/pages/product/order.jsx
@@ -114,11 +114,15 @@ const Order = () => {
               <div style={{margin: '10px 0' , width: '100%', height: '2px', backgroundColor: 'var(--primary)'}}/>
               <div style={{margin: '5px', fontFamily: 'var(--quicksand)', fontSize: '1rem', color: 'var(--text)', lineHeight : '30px'}}>
                 <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                  <div>Discount : </div>
+                  <div>{convertPrice(0)}</div>
+                </div>
+                <div style={{display: 'flex', justifyContent: 'space-between'}}>
                   <div>Price : </div>
                   <div>{convertPrice(i.price)}</div>
                 </div>
                 <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                  <div>Tax : </div>
+                  <div>Taxes & Fees : </div>
                   <div>{convertPrice(i.price * 0.11)}</div>
                 </div>
               </div>
@@ -132,11 +136,12 @@ const Order = () => {
             </div>
             <div className='prev-form'>
               <div className='itext'>Product</div>
-              <div className='product-card'>
-                <LazyLoadImage className='product-img' src={data.img} loading='lazy' effect='blur'/>
+              <div className="product-card" style={{ height: 'max-content', width: '100%', marginTop: '0px', justifyContent: 'center' }}>
+                <LazyLoadImage style={{ width: '100%' }} className='product-img' src={data.img} loading='lazy' effect='blur'/>
+              </div>
+              <div className='product-card' style={{ height: 'max-content', width: '100%', marginTop: "20px" }}>
                 <div className='wrapped-text'>
-                    <div className='product-title'>{data.title}</div>
-                    <div className='product-desc' style={{marginTop: '10px'}}>{data.desc}</div>
+                    <div className='product-title' style={{ fontSize: '1.4rem', textAlign: 'center' }}>{i.title}</div>
                 </div>
               </div>
             </div>
