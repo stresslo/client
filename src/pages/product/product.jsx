8f055219e45013a3d448599461485923e22cc25a
stresslo <stresslo.lab@gmail.com>
2024-03-03 18:28:21 +0700

    upload file

--- src/pages/product/product.jsx
+++ src/pages/product/product.jsx
@@ -69,34 +69,10 @@ const Product = () => {
         finally { setLoading(false) }
     }
 
-    const getFilteredData = async () => {
-        const endpoint = `${import.meta.env.VITE_API}/product/filter/${ctg}/${filterHistory.pricing}/${filterHistory.tech}/${filterHistory.price}/${filterHistory.optprice}/${page}`
-        if (filterHistory.tech || filterHistory.price || filterHistory.pricing || filterHistory.optprice) {
-            try {
-                setLoading(true)
-                const response = await axios.get(endpoint)
-                setUpdate(true)
-                setData(response.data)
-            } catch (error) {
-                return false;
-            } finally {
-                setLoading(false)
-            }
-        } else {
-            return false;
-        }
-    }
-
     useEffect(() => { 
         if (value) {
             search.show()
             searchProduct()
-        }
-    }, [])
-
-    useEffect(() => {
-        if (filterHistory) {
-            getFilteredData()
         }
     }, [])
 
