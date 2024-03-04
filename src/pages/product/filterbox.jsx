98d5dad4aff9c0d20b6784080c5c8988f90fb405
stresslo <stresslo.lab@gmail.com>
2024-03-04 08:47:10 +0700

    upload file

--- src/pages/product/filterbox.jsx
+++ src/pages/product/filterbox.jsx
@@ -43,7 +43,7 @@ const FilterBox = ({ filterHistory , setLoading, setUpdate, setData, page, ctg }
                 <div onClick={() => { document.querySelector('.filter-box').classList.remove('show') }} className="fa-solid fa-close fa-2xl" style={{color: 'var(--text)', cursor: 'pointer'}}/>
             </div>
             <div style={{width: '100%', height: 'max-content', marginTop: '20px'}}>
-                <div className="itext" style={{fontSize: '1.2rem', color: 'var(--blue)'}}>Pricing</div>
+                <div className="itext" style={{fontSize: '1.2rem', color: 'var(--text)'}}>Pricing</div>
                 <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px', padding: '15px 0px', paddingBottom: '35px'}}>
                     <div className="button" style={ pricing === 'all' ? {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)', color : 'var(--yellow)'} : {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'unset', border: '1px solid var(--blue)', color : 'var(--blue)'}} onClick={() => {setPricing('all'); price !== 0 && setPrice(0); setOptprice('all')}}>All</div>
                     <div className="button" style={ pricing === 'paid' ? {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)', color : 'var(--oren)'} : {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'unset', border: '1px solid var(--blue)', color : 'var(--blue)'}} onClick={() => {setPricing('paid'); setPrice(10000); setOptprice('under')}}>Paid</div>
@@ -51,27 +51,27 @@ const FilterBox = ({ filterHistory , setLoading, setUpdate, setData, page, ctg }
                 </div>
                 {(pricing === 'paid') && (
                 <>
-                <div className="itext" style={{fontSize: '1.2rem', color: 'var(--blue)'}}>Price Range</div>
+                <div className="itext" style={{fontSize: '1.2rem', color: 'var(--text)'}}>Price Range</div>
                 <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px', padding: '15px 0'}}>
                     <div className="button" style={ optprice === 'under' ? {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)', color : 'var(--green)'} : {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'unset', border: '1px solid var(--blue)', color : 'var(--blue)'}} onClick={() => setOptprice('under')}>Under</div>
                     <div className="button" style={ optprice === 'above' ? {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'var(--background)', color : 'var(--oren)'} : {borderRadius: '5px', width: '90px', height: '35px', boxShadow: 'unset', backgroundColor: 'unset', border: '1px solid var(--blue)', color : 'var(--blue)'}} onClick={() => setOptprice('above')}>Above</div>
                 </div>
                 <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', gap: '10px', paddingBottom: '35px'}}>
                     <input type="range" value={price} min={10000} max={1000000} step={5000} onChange={(e) => setPrice(e.target.value)} style={{width: '400px'}}/>
-                    <div className="itext" style={{fontSize: '0.95rem', color: 'var(--blue)'}}>{convertPrice(price)}</div>
+                    <div className="itext" style={{fontSize: '0.95rem', color: 'var(--text)'}}>{convertPrice(price)}</div>
                 </div>
                 </>
                 )}
                 {(ctg == 'web') && <>
-                <div className="itext" style={{fontSize: '1.2rem', color: 'var(--blue)'}}>Framework</div>
+                <div className="itext" style={{fontSize: '1.2rem', color: 'var(--text)'}}>Framework</div>
                 <div style={{display: 'flex', flexWrap: 'wrap', gap: '10px', padding: '15px 0px', paddingBottom: '35px'}}>
-                    <div className="button" style={ tech === 'all' ? {borderRadius: '10px', width: 'max-content', padding: '15px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--text)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '15px 20px', boxShadow: 'unset', backgroundColor: '#aaa', color : '#ccc'}} onClick={() => setTech('all')}>All</div>
-                    <div className="button" style={ tech === 'react' ? {borderRadius: '10px', width: 'max-content', padding: '15px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--text)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '15px 20px', boxShadow: 'unset', backgroundColor: '#aaa', color : '#ccc'}} onClick={() => setTech('react')}>React JS</div>
-                    <div className="button" style={ tech === 'angular' ? {borderRadius: '10px', width: 'max-content', padding: '15px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--text)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '15px 20px', boxShadow: 'unset', backgroundColor: '#aaa', color : '#ccc'}} onClick={() => setTech('angular')}>Angular JS</div>
-                    <div className="button" style={ tech === 'html' ? {borderRadius: '10px', width: 'max-content', padding: '15px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--text)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '15px 20px', boxShadow: 'unset', backgroundColor: '#aaa', color : '#ccc'}} onClick={() => setTech('html')}>HTML & CSS</div>
-                    <div className="button" style={ tech === 'vue' ? {borderRadius: '10px', width: 'max-content', padding: '15px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--text)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '15px 20px', boxShadow: 'unset', backgroundColor: '#aaa', color : '#ccc'}} onClick={() => setTech('vue')}>Vue JS</div>
-                    <div className="button" style={ tech === 'svelte' ? {borderRadius: '10px', width: 'max-content', padding: '15px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--text)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '15px 20px', boxShadow: 'unset', backgroundColor: '#aaa', color : '#ccc'}} onClick={() => setTech('svelte')}>Svelte JS</div>
-                    <div className="button" style={ tech === 'next' ? {borderRadius: '10px', width: 'max-content', padding: '15px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--text)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '15px 20px', boxShadow: 'unset', backgroundColor: '#aaa', color : '#ccc'}} onClick={() => setTech('next')}>Next JS</div>
+                    <div className="button" style={ tech === 'all' ? {borderRadius: '10px', width: 'max-content', padding: '20px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--primary)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '20px 20px', boxShadow: 'unset', backgroundColor: 'var(--prime)', color : '#aaa'}} onClick={() => setTech('all')}>All</div>
+                    <div className="button" style={ tech === 'react' ? {borderRadius: '10px', width: 'max-content', padding: '20px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--primary)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '20px 20px', boxShadow: 'unset', backgroundColor: 'var(--prime)', color : '#aaa'}} onClick={() => setTech('react')}>React JS</div>
+                    <div className="button" style={ tech === 'angular' ? {borderRadius: '10px', width: 'max-content', padding: '20px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--primary)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '20px 20px', boxShadow: 'unset', backgroundColor: 'var(--prime)', color : '#aaa'}} onClick={() => setTech('angular')}>Angular JS</div>
+                    <div className="button" style={ tech === 'html' ? {borderRadius: '10px', width: 'max-content', padding: '20px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--primary)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '20px 20px', boxShadow: 'unset', backgroundColor: 'var(--prime)', color : '#aaa'}} onClick={() => setTech('html')}>HTML & CSS</div>
+                    <div className="button" style={ tech === 'vue' ? {borderRadius: '10px', width: 'max-content', padding: '20px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--primary)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '20px 20px', boxShadow: 'unset', backgroundColor: 'var(--prime)', color : '#aaa'}} onClick={() => setTech('vue')}>Vue JS</div>
+                    <div className="button" style={ tech === 'svelte' ? {borderRadius: '10px', width: 'max-content', padding: '20px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--primary)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '20px 20px', boxShadow: 'unset', backgroundColor: 'var(--prime)', color : '#aaa'}} onClick={() => setTech('svelte')}>Svelte JS</div>
+                    <div className="button" style={ tech === 'next' ? {borderRadius: '10px', width: 'max-content', padding: '20px 20px', height: '0', boxShadow: 'unset', backgroundColor: 'var(--yellow)', color : 'var(--primary)'} : {borderRadius: '10px', width: 'max-content', height: '0', padding: '20px 20px', boxShadow: 'unset', backgroundColor: 'var(--prime)', color : '#aaa'}} onClick={() => setTech('next')}>Next JS</div>
                 </div>
                 </>}
                 <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', paddingTop: '20px'}}>
@@ -84,7 +84,7 @@ const FilterBox = ({ filterHistory , setLoading, setUpdate, setData, page, ctg }
                             setOptprice('all')
                         }}>Reset</div>
                         )}
-                        <div className="button-max" onClick={() => getFilteredData()} style={(tech !== 'all' || price || pricing !== 'all' ) ? {backgroundColor: 'var(--yellow)', borderRadius: '30px', height: '40px'} : { backgroundColor: '#aaa', height: '40px' }}>Apply</div>
+                        <div className="button-max" onClick={() => getFilteredData()} style={(tech !== 'all' || price || pricing !== 'all' ) ? {backgroundColor: 'var(--yellow)', borderRadius: '30px', height: '40px', color: 'var(--primary)'} : { backgroundColor: 'var(--prime)', height: '40px' , color: 'var(--primary)'}}>Apply</div>
                     </div>
                 </div>
             </div>
