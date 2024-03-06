fcfeedb91f6f6cb71562e91ae7d26e6a7c4f510a
stresslo <stresslo.lab@gmail.com>
2024-03-07 01:42:16 +0700

    upload file

--- src/pages/user/profile.jsx
+++ src/pages/user/profile.jsx
@@ -61,7 +61,7 @@ const Profile = () => {
     return (
         <div className='page' style={{flexDirection: 'column', gap : '10px'}}>
             <Topback/>
-            <LazyLoadImage loading='lazy' effect='blur' onClick={() => inputref.current.click()} src={(file) ? URL.createObjectURL(file) : context.img} width={155} height={155} style={{borderRadius : '50%', objectFit: 'cover', cursor : 'pointer'}}/>
+            <LazyLoadImage loading='lazy' effect='blur' onClick={() => inputref.current.click()} src={(file) ? URL.createObjectURL(file) : context.img} width={155} height={155} style={{borderRadius : '50%', objectFit: 'cover', border: '1.5px solid var(--yellow)', cursor : 'pointer'}}/>
             <div className='title'>{context.username}</div>
             <form style={{display: 'flex', alignItems: "center", flexDirection: 'column'}}>
                 <input type="file" onChange={(e) => setFile(e.target.files[0])} ref={inputref} style={{display: 'none'}}/>
