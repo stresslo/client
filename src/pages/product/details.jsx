872e7909d7908401f1f324338f87138f0be020e1
stresslo <stresslo.lab@gmail.com>
2024-03-08 15:28:28 +0700

    upload file

--- src/pages/product/details.jsx
+++ src/pages/product/details.jsx
@@ -136,7 +136,7 @@ const Details = () => {
                         <div className='product-card' style={{ height: 'max-content', width: '100%' }}>
                             <div className='wrapped-text'>
                                 <div className='product-title' style={{ fontSize: '1.4rem' }}>More Features</div>
-                                <div className='product-desc' style={{ display: "block", fontSize: '1rem', marginTop: '10px', fontFamily: 'var(--quicksand)', color: 'var(--blue)' }}>{i.extra}</div>
+                                <pre className='product-desc' style={{ display: "block", fontSize: '1rem', marginTop: '10px', fontFamily: 'var(--quicksand)', color: 'var(--blue)' }}>{i.extra}</pre>
                             </div>
                         </div>
                         }
