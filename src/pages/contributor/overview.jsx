5e0f610b215336fcfa0c823c4ab319f06f6ac928
stresslo <stresslo.lab@gmail.com>
2024-03-10 01:37:00 +0700

    upload file

--- src/pages/contributor/overview.jsx
+++ src/pages/contributor/overview.jsx
@@ -83,10 +83,17 @@ const Overview = () => {
                     }
                     <div className="button" style={{zIndex: '2', boxShadow: 'var(--boxshadow)',width: '135px', fontSize: '0.95rem', fontFamily: 'var(--quicksand)' ,height: '47px', borderRadius: '10px', backgroundColor: 'var(--primary)', color: 'var(--text)'}}>{bank || 'Bank'}</div>
                     <input ref={refnumber} id="rek_bank" type="text" style={{backgroundColor: 'var(--primary)', zIndex: '2', color: 'var(--text)', fontSize: '0.9rem', cursor: 'text'}} className="button-max" value={rekening} onChange={(e) => setRekening(e.target.value)} placeholder="rekening number" readOnly/>
-                    <div style={{color: 'var(--text)', cursor: 'pointer', marginLeft: '10px'}} onClick={() => handle.editBank()} className="fa-solid fa-pen-to-square fa-2xl"/>
+                    <div style={{color: 'var(--text)', cursor: 'pointer', marginLeft: '15px'}} onClick={() => handle.editBank()} className="fa-solid fa-pen-to-square fa-xl"/>
                 </div>
                 {(data.bank_name !== bank || data.bank_number !== rekening) &&
-                <div className="button-max" style={{marginTop: '20px', backgroundColor: 'var(--yellow)', boxShadow: 'var(--boxshadow)'}}>Save change</div>
+                <div style={{display: 'flex', gap: '10px'}}>
+                    <div onClick={() => {
+                        const rek_bank = document.getElementById('rek_bank')
+                        rek_bank.setAttribute('readonly')
+                        setEditBank(false)
+                    }} className="button-max" style={{boxShadow: 'var(--boxshadow)'}}>Cancel</div>
+                    <div className="button-max" style={{backgroundColor: 'var(--yellow)', boxShadow: 'var(--boxshadow)'}}>Save change</div>
+                </div>
                 }
                 </>
                 }
