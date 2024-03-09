ad2c4825e5cbaa29db47c7ef05df2779003c4b25
stresslo <stresslo.lab@gmail.com>
2024-03-10 00:30:28 +0700

    upload file

--- src/pages/contributor/overview.jsx
+++ src/pages/contributor/overview.jsx
@@ -58,7 +58,7 @@ const Overview = () => {
                     }
                 </div>
                 <div style={{width: '100%', display: 'flex', gap: '10px', marginTop: '10px'}}>
-                    <select ref={bankref} style={{position: 'absolute', opacity: '0', left: '0', zIndex: '0'}} value={bank} onChange={(e) => setBank(e.target.value)} required>
+                    <select ref={bankref} style={{position: 'absolute', opacity: '0', left: '0', zIndex: '3'}} value={bank} onChange={(e) => setBank(e.target.value)} required>
                         <option value="" disabled>-- Select Bank --</option>
                         <option value="BCA">BCA</option>
                         <option value="Mandiri">Mandiri</option>
