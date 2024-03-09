90e870ea8c966d76b7095d507e634672b8edff8a
stresslo <stresslo.lab@gmail.com>
2024-03-09 17:31:44 +0700

    upload file

--- src/pages/contributor/overview.jsx
+++ src/pages/contributor/overview.jsx
@@ -42,7 +42,10 @@ const Overview = () => {
                         <img onClick={() => navigate('/profile')} src={context.img} style={{cursor: 'pointer', width: '35px', border: '2px solid var(--yellow)', height: '35px', objectFit: 'cover', borderRadius: '50%', boxSizing: 'border-box', boxShadow: 'var(--boxshadow)'}} alt="stresslo account" />
                         <div style={{fontSize: '1.2rem', color: 'var(--yellow)',}}>{data.username}</div>
                     </div>
-                    <div style={{fontSize: '1.1rem', fontFamily: 'var(--poppins)', color: 'var(--text)', marginTop: '10px'}}>Balance : {convertPrice(data.amount)}</div>
+                    <div style={{display: 'flex', alignItems: 'center', 'gap' : '10px'}}>
+                        <div style={data.status == 'review' ? {backgroundColor: 'var(--oren)'} : {backgroundColor: 'var(--green)'}}>{data.status}</div>
+                    </div>
+                    <div style={{fontSize: '1.2rem', fontFamily: 'var(--poppins)', color: 'var(--text)', marginTop: '5px'}}>Balance : {convertPrice(data.amount)}</div>
                     <div className="button contact">Withdraw</div>
                 </div>
                 }
