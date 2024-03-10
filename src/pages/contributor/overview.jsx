c43753f3aa8f528871446e8e2a3b293cb7478b4e
stresslo <stresslo.lab@gmail.com>
2024-03-10 14:57:59 +0700

    upload file

--- src/pages/contributor/overview.jsx
+++ src/pages/contributor/overview.jsx
@@ -45,6 +45,8 @@ const Overview = () => {
                 headers: {'xsrf-token' : vxsrf}
             })
             swalert(response.data, 'success', 3000)
+            const rek_bank = document.getElementById('rek_bank')
+            rek_bank.setAttribute('readonly', true)
         } catch (error) {
             error || error.response && swalert(error.response.data, 'info', 3000)
         }
