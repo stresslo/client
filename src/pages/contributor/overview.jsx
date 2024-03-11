7000ad03508ceddc91ca364d51ed4744b6e99225
stresslo <stresslo.lab@gmail.com>
2024-03-12 03:02:27 +0700

    upload file

--- src/pages/contributor/overview.jsx
+++ src/pages/contributor/overview.jsx
@@ -187,7 +187,7 @@ const Overview = () => {
                             </div>
                             )
                         }): 
-                        <div className="title" style={{fontSize: '0.8rem', margin: 'auto', fontFamily: 'var(--quicksand)'}}>You don't have product data yet.</div>}
+                        <div className="title" style={{fontSize: '0.8rem', margin: 'auto', fontFamily: 'var(--poppins)'}}>You don't have product data yet.</div>}
                     </div>
                     <div className="overview-product" style={{flexDirection: 'column', marginTop: '10px'}}>
                         <div style={{fontFamily: 'var(--poppins)', color: 'var(--text)', fontSize: '0.9rem'}}>Total product : {data.total_product}</div>
@@ -201,17 +201,17 @@ const Overview = () => {
                 <div className="itext" style={{marginTop: '50px'}}>Withdraw</div>
                 <div style={{position : 'relative'}}>
                     <div style={{color : 'var(--text)', fontFamily: 'var(--poppins)', fontSize: '0.8rem', position: 'absolute', top: '-5px', right: '5px', cursor: 'pointer' }}>See all</div>
-                    <div className="overview-product" style={withdraw.length !== 0 ? {marginTop: '25px', height: 'max-content'} : {marginTop: '25px', height: '150px'}}>
+                    <div className="overview-product" style={withdraw.length !== 0 ? {marginTop: '25px', height: '200px', overflow: 'hidden scroll'} : {marginTop: '25px', height: '200px'}}>
                         {(withdraw.length != 0) ? 
                         withdraw.map((i, key) => {
                             return (
-                            <div key={key} className="overview-product-card" style={{flexDirection: 'column'}}>
-                                <LazyLoadImage src={i.img} style={{height: '100px', width: '150px', objectFit: 'cover', borderRadius: '5px'}}/>
-                                <div style={{color : 'var(--yellow)', fontSize: '0.9rem', textAlign: 'center'}}>{i.paid}x download</div>
+                            <div key={key} style={{width: '100%', height: 'max-content', backgroundColor: 'var(--primary)', boxShadow: 'var(--boxshadow)', borderRadius: '5px', padding: '10px'}}>
+                                <div style={{color : 'var(--yellow)', fontSize: '1rem', fontFamily: 'var(--poppins)'}}>{i.total}</div>
+                                <div style={{color : 'var(--blue)', fontSize: '0.8rem', fontFamily: 'var(--poppins)'}}>date : {i.date} {i.time}</div>
                             </div>
                             )
                         }): 
-                        <div className="title" style={{fontSize: '0.8rem', margin: 'auto', fontFamily: 'var(--quicksand)'}}>No withdrawal history.</div>}
+                        <div className="title" style={{fontSize: '0.8rem', margin: 'auto', fontFamily: 'var(--poppins)'}}>No withdrawal history.</div>}
                     </div>
                     <div className="overview-product" style={{flexDirection: 'column', marginTop: '10px'}}>
                         <div style={{fontFamily: 'var(--poppins)', color: 'var(--text)', fontSize: '0.9rem'}}>Total income : {convertPrice(data.total_income)}</div>
