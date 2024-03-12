99ac00f1c69cbbbb5834dab8197a7397ac67cd39
stresslo <stresslo.lab@gmail.com>
2024-03-12 21:13:34 +0700

    upload file

--- src/pages/contributor/overview.jsx
+++ src/pages/contributor/overview.jsx
@@ -213,7 +213,7 @@ const Overview = () => {
                             <div key={key} style={{width: '100%', height: 'max-content', backgroundColor: 'var(--primary)', boxShadow: 'var(--boxshadow)', borderRadius: '5px', padding: '10px', boxSizing: 'border-box'}}>
                                 <div style={{display: 'flex', alignItems: 'center', gap: '5px'}}>
                                     <div style={{color : 'var(--yellow)', fontSize: '1rem', fontFamily: 'var(--poppins)'}}>{convertPrice(i.total)}</div>
-                                    <div className="overview status" style={{color: 'var(--green)'}}>success</div>
+                                    <div className="overview-status" style={{color: 'var(--green)'}}>success</div>
                                 </div>
                                 <div style={{color : 'var(--blue)', fontSize: '0.8rem', fontFamily: 'var(--poppins)'}}>{i.date} {i.time}</div>
                             </div>
