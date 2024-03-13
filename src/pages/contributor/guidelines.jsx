6f0f425b9c066921aa5b16a6e1a9b815e7e6f167
stresslo <stresslo.lab@gmail.com>
2024-03-13 23:36:17 +0700

    upload file

--- src/pages/contributor/guidelines.jsx
+++ src/pages/contributor/guidelines.jsx
@@ -5,55 +5,55 @@ const Guidelines = () => {
     return (
         <div className='page-max'>
             <Topback location={-1}/>
-            <div className='form' style={{flexDirection: 'column', textAlign: 'left', fontFamily: 'var(--poppins)', color: 'var(--text)', fontSize: '0.75rem', gap: '10px', marginTop: '30px'}}>
+            <div className='form' style={{flexDirection: 'column', textAlign: 'left', fontFamily: 'var(--poppins)', color: 'var(--blue)', fontSize: '0.75rem', gap: '15px', marginTop: '80px'}}>
                 <h1><span>Welcome to </span> the <span>Contributor</span> Community!</h1>
                 <p>
                 Thank you for your interest in contributing to our project. To ensure a fair and transparent experience for all contributors, please review and adhere to the following guidelines:
                 </p>
         
-                <h2 style={{marginTop: '10px'}}>1. Content Contribution:</h2>
+                <h2 style={{marginTop: '20px'}}>1. <span>Content</span> Contribution:</h2>
                 <ul>
                 <li>Your submitted content should be original, non-copyright infringing, and relevant to the project's topic.</li>
                 <li>Avoid content that is offensive, discriminatory, or violates ethical standards.</li>
                 </ul>
         
-                <h2>2. Revenue Sharing:</h2>
+                <h2>2. <span>Revenue</span> Sharing:</h2>
                 <ul>
                 <li>Contributors will receive 90% of the total revenue generated from the sale of their content.</li>
                 <li>We reserve 10% of the total sales as a contribution for project support and development.</li>
                 </ul>
         
-                <h2>3. Payment:</h2>
+                <h2>3. <span>Pay</span>ment:</h2>
                 <ul>
                 <li>Contributor payments will be processed regularly according to the established schedule.</li>
                 <li>Ensure that your payment information is valid to receive compensation.</li>
                 </ul>
         
-                <h2>4. Content Quality:</h2>
+                <h2>4. <span>Content</span> Quality:</h2>
                 <ul>
                 <li>Submitted content should meet the established quality standards of the project.</li>
                 <li>Content not meeting the criteria may be rejected or requested for revision.</li>
                 </ul>
         
-                <h2>5. Payment Verification:</h2>
+                <h2>5. <span>Payment</span> Verification:</h2>
                 <ul>
                 <li>Contributors have access to sales and earnings reports through their personal dashboard.</li>
                 <li>All payment transparency and information will be detailed for contributors.</li>
                 </ul>
         
-                <h2>6. Recognition and Awards:</h2>
+                <h2>6. <span>Recognition</span> and Awards:</h2>
                 <ul>
                 <li>Outstanding and consistent contributors will receive recognition and awards within the community.</li>
                 <li>Decisions on awards will be based on contribution quantity and quality.</li>
                 </ul>
         
-                <h2>7. Changes to Terms:</h2>
+                <h2>7. <span>Changes</span> to Terms:</h2>
                 <ul>
                 <li>These terms and conditions may be updated over time to ensure fairness and alignment with project developments.</li>
                 <li>Contributors will be notified of significant policy changes.</li>
                 </ul>
         
-                <h2 style={{marginTop: '10px'}}>Questions and Assistance:</h2>
+                <h2 style={{marginTop: '20px'}}> <span>Questions</span> and Assistance:</h2>
                 <p>
                 If you have any questions or need assistance, please contact our project management team.
                 </p>
