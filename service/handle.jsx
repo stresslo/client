2384c4b0ced0064d6e21752d11d6feea9e0040d2
stresslo <stresslo.lab@gmail.com>
2024-03-13 21:07:19 +0700

    upload file

--- service/handle.jsx
+++ service/handle.jsx
@@ -6,9 +6,15 @@ const Handle = ({ status }) => {
         <div className="page" style={{flexDirection: 'column', gap: '30px'}}>
             <Topback/>
             {(status == 404) ? (
-                <LazyLoadImage src="/img/404page.png" style={{width: '220px'}} effect="blur" loading="lazy"/>
+                <>
+                    <LazyLoadImage src="/img/404page.png" style={{width: '220px'}} effect="blur" loading="lazy"/>
+                    <div style={{fontFamily: 'var(--poppins)', color: 'var(--text)', fontSize: '1.1rem'}}> <span>Product</span> is empty</div>
+                </>
             ) : (
-                <LazyLoadImage src="/img/500.png" style={{width: '270px'}} effect="blur" loading="lazy"/>
+                <>
+                    <LazyLoadImage src="/img/500.png" style={{width: '270px'}} effect="blur" loading="lazy"/>
+                    <div style={{fontFamily: 'var(--poppins)', color: 'var(--text)', fontSize: '1.1rem'}}> <span>Server</span> Maintenance</div>
+                </>
             )}
         </div>
     )
